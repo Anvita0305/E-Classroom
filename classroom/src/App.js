3e9889ce2b4d70ca3ddbd8a9a2f089661a555b2c
Anvita Mahajan <78889572+Anvita0305@users.noreply.github.com>
2022-04-17 17:02:33 +0530

    Update App.js

--- classroom/src/App.js
+++ classroom/src/App.js
@@ -1,51 +1,21 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Register from "./Components/Navbar/mainRegister";
+import Login from "./Components/Navbar/mainLogin";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+	
+return (
+	<>
+	<BrowserRouter>
+	<Routes>
+		<Route exact path="/" element={<Register/>}/>
+		<Route exact path="/Login" element={<Login/>}/>
+	</Routes>
+	</BrowserRouter>
+	</>
+);
+}
 
-// export default App;
+export default App;
 
-
-// import React from 'react';
-// import './App.css';
-// import Navbar from './components/Navbar';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Home from './pages';
-// import About from './pages/about';
-// import Blogs from './pages/blogs';
-// import SignUp from './pages/signup';
-
-// function App() {
-// return (
-// 	<Router>
-// 	<Navbar />
-// 	<Switch>
-// 		<Route path='/' exact component={Home} />
-// 		<Route path='/about' component={About} />
-// 		<Route path='/blogs' component={Blogs} />
-// 		<Route path='/sign-up' component={SignUp} />
-// 	</Switch>
-// 	</Router>
-// );
-// }
-
-// export default App;
